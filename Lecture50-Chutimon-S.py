603ff47866cab9282d5c106a2668a8fb3c85e096
CaPloy <chutimon1104@gmail.com>
2023-04-30 13:59:50 +0700

    Create Lecture50-Chutimon-S.py

---
+++ Lecture50-Chutimon-S.py
@@ -0,0 +1,12 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def times(x,y):
+    print(x*y)
+def division(x,y):
+    print(x/y)
+plus(100,20)
+minus(100,20)
+times(100,20)
+division(100,20)
